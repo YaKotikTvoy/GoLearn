87e21e233c843b761764941fe9ce8bcfaa0faf30
YaKotikTvoy <YaKotikTvoy@yandex.ru>
2025-12-14 21:01:05 +0500

    2.1 Programm structure

---
+++ LessonsMetanit/Chapter2/2.1 Programm structure/main.go
@@ -0,0 +1,18 @@
+/*
+    Первая программа 
+    на языке Go
+*/
+package main // Определение пакета для текущего файла
+
+import "fmt" // Подключение пакета fmt
+
+func main() {
+	fmt.Println("Hello METANIT.COM!")
+	fmt.Println("Hello Golang!")
+	fmt.Println("Hello METANIT.COM!")
+
+	//Интересненько =D Можно размещать несколько инструкций на одной строке, то
+	//Тогда их нужно разделять точкой запятой ";"
+	fmt.Println("Hello METANIT.COM!"); fmt.Println("Hello Golang!"); fmt.Println("Hello METANIT.COM!")
+	
+}
